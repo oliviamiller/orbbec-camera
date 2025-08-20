0d431bad3fe51a90daa28a307db69a483111d670
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2025-08-20 16:06:30 -0400

    set up admin priv

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,6 +1,14 @@
 @echo on
 setlocal enabledelayedexpansion
 
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 echo Checking dependencies...
 
 where choco > nul 2>&1
